dabfa0ccc0708335f790e5c6ba309467b649a4e4
Halil İbrahim Yanık <31934322+halilyanik98@users.noreply.github.com>
2022-03-15 10:10:56 +0300

    add pipe

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,6 +1,17 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsNumberString } from "class-validator";
+
 export class CreateTaskDto {
     id:number;
     firstName:string;
     lastName:string;
+    age:number;
+    //Pipo Kullanılmıştır
+
+    @IsEmail()
+    mail:string;
+    //Pipo Kullanılmıştır
+    
+    @IsNotEmpty()
+    password:string;
     newTask:string;
 }
