85d68614251fdc9ee0357ff6e81607db65ac695a
Halil İbrahim Yanık <31934322+halilyanik98@users.noreply.github.com>
2022-03-15 10:12:30 +0300

    Update main.ts
    add pipe

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //pipo kullanımı için eklenmiştir.
+  app.useGlobalPipes(new ValidationPipe({
+    disableErrorMessages: true,
+  }))
   await app.listen(3000);
 }
 bootstrap();
